6b412c9d61a772fe935cce864fe107470d486aee
Victor MORALES <victor.morales@outlook.fr>
2025-06-06 12:09:04 +0200

    GREEN

--- tp-tdd-renew-subscription/subscription.js
+++ tp-tdd-renew-subscription/subscription.js
@@ -1,10 +1,12 @@
 function canRenewSubscription(subscription, currentDate) {
-  const { hasBeenRenewed, endDate } = subscription;
+  const { status, hasBeenRenewed, endDate } = subscription;
   const datePassed = new Date(endDate) <= new Date(currentDate);
+  if (status !== 'active') return false;
   if (hasBeenRenewed) return false;
   if (!datePassed) return false;
   return true;
 }
+
 
 
 
