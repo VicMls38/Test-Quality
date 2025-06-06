bae1c1027bae9689df18e8a89a0a553ac6afa921
Victor MORALES <victor.morales@outlook.fr>
2025-06-06 12:03:17 +0200

    RED

--- tp-tdd-renew-subscription/subscription.test.js
+++ tp-tdd-renew-subscription/subscription.test.js
@@ -11,3 +11,15 @@ test('should return true when all conditions are met', () => {
   const today = '2025-06-06';
   expect(canRenewSubscription(subscription, today)).toBe(true);
 });
+
+test('Abonnement déjà renouvelé → false', () => {
+  const subscription = {
+    status: 'active',
+    endDate: '2025-06-01',
+    hasBeenRenewed: true,
+    unpaidDebt: false,
+    isTrial: false,
+  };
+  const currentDate = '2025-06-06';
+  expect(canRenewSubscription(subscription, currentDate)).toBe(false);
+});
