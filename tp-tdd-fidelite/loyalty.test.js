a2e311688217e8ac68808c781d396bf66f57899b
Victor MORALES <victor.morales@outlook.fr>
2025-06-06 11:13:35 +0200

    RED

--- tp-tdd-fidelite/loyalty.test.js
+++ tp-tdd-fidelite/loyalty.test.js
@@ -4,4 +4,14 @@ describe('calculateLoyaltyPoints', () => {
   test('should return 0 for empty cart', () => {
     expect(calculateLoyaltyPoints([])).toBe(0);
   });
+
+  test('should return 1 point for a standard product costing 10€', () => {
+    const cart = [{ type: 'standard', price: 10 }];
+    expect(calculateLoyaltyPoints(cart)).toBe(1);
+  });
+
+  test('should return 3 points for a standard product costing 35€', () => {
+    const cart = [{ type: 'standard', price: 35 }];
+    expect(calculateLoyaltyPoints(cart)).toBe(3); // car Math.floor(35/10) = 3
+  });
 });
