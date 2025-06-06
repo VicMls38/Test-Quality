bd3711ebde3997a7c2bd5ba7e73d4a1403221a0a
Victor MORALES <victor.morales@outlook.fr>
2025-06-06 11:14:26 +0200

    GREEN

--- tp-tdd-fidelite/loyalty.js
+++ tp-tdd-fidelite/loyalty.js
@@ -1,6 +1,15 @@
 function calculateLoyaltyPoints(cart) {
   if (!Array.isArray(cart) || cart.length === 0) return 0;
 
+  let points = 0;
+
+  for (const item of cart) {
+    if (item.type === 'standard' && typeof item.price === 'number' && item.price > 0) {
+      points += Math.floor(item.price / 10);
+    }
+  }
+
+  return points;
 }
 
 module.exports = { calculateLoyaltyPoints };
