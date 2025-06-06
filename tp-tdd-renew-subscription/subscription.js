1fd5bd49d1057f69e1275ea7373b865d4b95247c
Victor MORALES <victor.morales@outlook.fr>
2025-06-06 12:08:09 +0200

    REFACTOR

--- tp-tdd-renew-subscription/subscription.js
+++ tp-tdd-renew-subscription/subscription.js
@@ -1,7 +1,8 @@
 function canRenewSubscription(subscription, currentDate) {
   const { hasBeenRenewed, endDate } = subscription;
+  const datePassed = new Date(endDate) <= new Date(currentDate);
   if (hasBeenRenewed) return false;
-  if (new Date(endDate) > new Date(currentDate)) return false;
+  if (!datePassed) return false;
   return true;
 }
 
